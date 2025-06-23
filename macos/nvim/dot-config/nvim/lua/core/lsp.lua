3e17b480b81db6542b9e2c2f2cf83eb8ba200dc1
xsn34kzx <xsn34kzx@gmail.com>
2025-06-23 19:57:13 -0400

    Add Kitty Setup

--- macos/nvim/dot-config/nvim/lua/core/lsp.lua
+++ macos/nvim/dot-config/nvim/lua/core/lsp.lua
@@ -20,7 +20,7 @@ vim.diagnostic.config({
             [vim.diagnostic.severity.ERROR] = "󰅚 ",
             [vim.diagnostic.severity.WARN] = "󰀪 ",
             [vim.diagnostic.severity.INFO] = "󰋽 ",
-            [vim.diagnostic.severity.HINT] = "󰌶 ",
+            [vim.diagnostic.severity.HINT] = "󰌶",
         },
         numhl = {
             [vim.diagnostic.severity.ERROR] = "ErrorMsg",
